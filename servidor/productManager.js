912c3a8ed260288a3f8051b3d7a5dcf346ed4227
unknown <facundo.cordoba2012@gmail.com>
2022-12-29 17:23:58 -0300

    Servidor express

---
+++ servidor/productManager.js
@@ -0,0 +1,105 @@
+const fs = require("node:fs");
+
+class ProductManager {
+    products;
+    id;
+
+    constructor(path) {
+        this.path = path;
+        this.loadFile();
+    }
+
+    loadFile() {
+        try {
+            this.products = JSON.parse(fs.readFileSync(this.path, "utf-8"));
+        } catch (err) {
+            throw new Error(err);
+        }
+        this.controlID()
+    }
+
+    updateProduct() {
+        try {
+            fs.writeFileSync(this.path, JSON.stringify(this.products));
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
+
+    controlID() {
+        this.id = this.products.length;
+    }
+
+    deleteProduct(id) {
+        let productVoid = { description: "Producto eliminado", id: id };
+        this.products[id - 1] = productVoid;
+        this.updateProduct();
+    }
+
+    addProducts(product) {
+        if (!product.stock && !product.title && !product.description && !product.price) {
+            console.log("Cuidado! Campos de un producto incompleto");
+        } else {
+            let searchCode = this.products.find((prod) => prod.code === product.code);
+            if (searchCode) {
+                console.log("ERROR, codigo del producto " + product.title + " ya existe");
+            } else {
+                this.id++;
+                product = { ...product, id: this.id };
+                this.products.push(product);
+                this.updateProduct();
+            }
+        }
+    }
+
+    getProducts() {
+        console.log("PRODUCTOS: ", this.products);
+    }
+
+    getProductsById(id) {
+        let prodForId = null;
+        this.products.findIndex((prod) => {
+            if (prod.id === id) {
+                prodForId = prod;
+            }
+        });
+        if (prodForId) {
+            console.log("El producto con id " + id + " es " + prodForId.title);
+        } else {
+            console.log("Id not found");
+        }
+    }
+
+    returnProducts() {
+        return this.products;
+    }
+}
+
+const manager = new ProductManager("./products.json");
+
+//----- Desarollo de los productos -----//
+// const product1 = { title: "Fanta", description: "Gaseosa", price: 400, thumbnail: "link", code: "ABC128", stock: 80 };
+// manager.addProducts(product1);
+
+// const product2 = { title: "Coca-Cola", description: "Gaseosa", price: 500, thumbnail: "link", code: "ABC124", stock: 50 };
+// manager.addProducts(product2);
+
+// const product3 = { title: "Goliat", description: "Gaseosa", price: 100, thumbnail: "link", code: "ABC126", stock: 50 };
+// manager.addProducts(product3);
+
+// // Producto 4 codigo repetido //
+// const product4 = { title: "Manaos", description: "Gaseosa", price: 100, thumbnail: "link", code: "ABC125", stock: 100 };
+// manager.addProducts(product4);
+
+// // Producto 5 incompleto //
+// const product5 = { thumbnail: "link", code: "ABC" };
+// manager.addProducts(product5);
+// // ----- ----- //
+
+//manager.deleteProduct(5);
+manager.getProducts();
+manager.getProductsById(6);
+
+module.exports = {
+    productManager: manager
+}
